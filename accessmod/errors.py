f8b6964bca6b573cd6fbe0a4f0a8db1f18a27b15
Yann Forget <yannforget@mailbox.org>
2022-04-19 08:32:25 +0200

    Add errors module

---
+++ accessmod/errors.py
@@ -0,0 +1,14 @@
+class AccessModError(Exception):
+    """AccessMod error."""
+
+
+class AccessModInputError(Exception):
+    """Invalid input data."""
+
+
+class AccessModOutputError(Exception):
+    """Invalid output data."""
+
+
+class AccessModProcessingError(Exception):
+    """Error during processing."""
